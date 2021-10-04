ec157c87ba78a7022097c8feda24d7883c723e67
Олег Монахов <olegate@yandex.ru>
2021-10-04 15:34:52 +0300

    Наброски

--- js/main.js
+++ js/main.js
@@ -46,3 +46,41 @@ const getRandomFloatInclusive = (fromStart, toFinish, signsAfter) => {
 
 getRandomFloatInclusive (0, 1, 5);
 
+//Генерация случайных данных по заданию
+const XX = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+const author = {
+  avatar: 'img/avatars/user.png',
+};
+
+const offer = {
+  title: 'Квартира со всем удобствами',
+  address: 'location.lat, location.lng',
+  price: 1000,
+  type: 'house',
+  rooms: 1,
+  guests: 0,
+  checkin: '12:00',
+  checkout: '13:00',
+  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  description: 'Квартира в самом центре города в 3 минутах ходьбы от метро',
+  photos: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'],
+};
+
+const location = {
+  lat: 35.65000,
+  lng: 139.70000,
+};
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomIntNumber(0, elements.length - 1)];
+};
+
+const createAuthor = () => {
+  let indexCut = getRandomArrayElement(XX);
+  XX.splice(+indexCut - 1);
+  return {
+    avatar: 'img/avatars/user' + indexCut  + '.png'
+  };
+};
+
+const varsin = createAuthor();
