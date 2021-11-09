3816b019dc18f080702cabd8b948f496fc9857be
Олег Монахов <olegate@yandex.ru>
2021-11-09 20:09:00 +0300

    Исправления внесены

--- js/main.js
+++ js/main.js
@@ -1,35 +1,17 @@
-// import {createSimilarOffers} from './data/get-mock-data.js';
-// import {getSimilarOfferElement} from './utils/get-similar-offer-elements.js';
+import {setNonActiveMode} from './form/set-non-active-mode.js';
+import {initMap} from './map/map-script.js';
+import {createSimilarOffers} from './data/get-mock-data.js';
 
-import {createSimilarPins, initMap} from './map/map-script.js';
-// import {setActiveMode} from './form/set-active-mode.js';
+const MAX_OFFERS_QUANTITY = 10;
 
-// const MAX_OFFERS_QUANTITY = 1;
+const adForm = document.querySelector('.ad-form');
+const filterPanel = document.querySelector('.map__filters');
+const formFieldsets = adForm.querySelectorAll('fieldset');
+const filterFieldsets = filterPanel.querySelectorAll('fieldset');
+const filterSelects = filterPanel.querySelectorAll('select');
 
-// const similarCards = createSimilarOffers(MAX_OFFERS_QUANTITY);
+setNonActiveMode(adForm, filterPanel, formFieldsets, filterFieldsets, filterSelects);
 
-// const similarListElement = document.querySelector('#map-canvas');
-// const similarListFragment = document.createDocumentFragment();
+const similarCards = createSimilarOffers(MAX_OFFERS_QUANTITY);
 
-// similarCards.forEach((similarCard) => {
-//   const outputCard = getSimilarOfferElement(similarCard);
-//   similarListFragment.appendChild(outputCard);
-// });
-
-// similarListElement.appendChild(similarListFragment);
-
-// const adForm = document.querySelector('.ad-form');
-// const filterPanel = document.querySelector('.map__filters');
-// const formFieldsets = adForm.querySelectorAll('fieldset');
-// const filterFieldsets = filterPanel.querySelectorAll('fieldset');
-// const filterSelects = filterPanel.querySelectorAll('select');
-
-// map.addEventListener('load', () => {
-//   console.log('Карта инициализирована2');
-//   // if (bestVar2) {
-//   console.log('Карта инициализирована3');
-//   setActiveMode(adForm, filterPanel, formFieldsets, filterFieldsets, filterSelects);
-//   // }
-// });
-initMap();
-createSimilarPins();
+initMap(similarCards);
--- js/map/map-script.js
+++ js/map/map-script.js
@@ -1,27 +1,102 @@
-import {createSimilarOffers} from '../data/get-mock-data.js';
 import {getSimilarOfferElement} from '../utils/get-similar-offer-elements.js';
-import {setNonActiveMode} from '../form/set-non-active-mode.js';
 import {setActiveMode} from '../form/set-active-mode.js';
 
-const MAX_OFFERS_QUANTITY = 10;
 const offerAddress = document.getElementById('address');
 const adForm = document.querySelector('.ad-form');
 const filterPanel = document.querySelector('.map__filters');
 const formFieldsets = adForm.querySelectorAll('fieldset');
 const filterFieldsets = filterPanel.querySelectorAll('fieldset');
 const filterSelects = filterPanel.querySelectorAll('select');
+const initCoordinates = {lat: 35.6895, lng: 139.692, zoom: 10};
 
-setNonActiveMode(adForm, filterPanel, formFieldsets, filterFieldsets, filterSelects);
+const map = L.map('map');
+const similarOffersMarkersLayer = L.layerGroup().addTo(map);
 
-const initMap = (map, mainPinMarker) => {
-  map = L.map('map')
-    .on('load', () => {
-      setActiveMode(adForm, filterPanel, formFieldsets, filterFieldsets, filterSelects);
-    })
-    .setView({
-      lat: 35.6895,
-      lng: 139.692,
-    }, 10);
+const mainPinIcon = L.icon({
+  iconUrl: '../img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 35.6895,
+    lng: 139.692,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.on('moveend', (evt) => {
+  const coordsPinMarker = evt.target.getLatLng();
+
+  offerAddress.value = `${coordsPinMarker.lat.toFixed(5)}, ${coordsPinMarker.lng.toFixed(5)}`;
+
+});
+
+mainPinMarker.addTo(map);
+
+const similarPinIcon = L.icon({
+  iconUrl: '../img/pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const createCustomPopup = (point) => {
+  const similarListElement = document.querySelector('#card').content.querySelector('.popup');
+  const popupElement = similarListElement.cloneNode(true);
+
+  const outputCard = getSimilarOfferElement(point);
+  popupElement.innerHTML='';
+
+  popupElement.append(outputCard);
+
+  return popupElement;
+};
+
+const setMapView = (lat, lng, zoom) => {
+  map.setView({
+    lat,
+    lng,
+  },
+  zoom,
+  );
+};
+
+const setMainPinMarkerCoordinates = (lat, lng) => {
+  mainPinMarker.setLatLng({
+    lat,
+    lng,
+  });
+};
+
+const renderSimilarCards = (similarCards) => {
+  similarOffersMarkersLayer.clearLayers();
+  similarCards.forEach((point) => {
+    const {locationCoords: {lat, lng}} = point;
+
+    const marker = L.marker({
+      lat,
+      lng,
+    },
+    {
+      icon: similarPinIcon,
+    });
+
+    marker
+      .addTo(similarOffersMarkersLayer)
+      .bindPopup(createCustomPopup(point));
+  });
+};
+
+const initMap = (similarCards) => {
+  map.on ('load', () => {
+    setActiveMode(adForm, filterPanel, formFieldsets, filterFieldsets, filterSelects);
+  });
+
+  setMapView (initCoordinates.lat, initCoordinates.lng, initCoordinates.zoom);
 
   L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -35,81 +110,14 @@ const initMap = (map, mainPinMarker) => {
     },
   ).addTo(map);
 
-  const mainPinIcon = L.icon({
-    iconUrl: '../img/main-pin.svg',
-    iconSize: [52, 52],
-    iconAnchor: [26, 52],
-  });
-
-  mainPinMarker = L.marker(
-    {
-      lat: 35.6895,
-      lng: 139.692,
-    },
-    {
-      draggable: true,
-      icon: mainPinIcon,
-    },
-  );
-
-  mainPinMarker.on('moveend', (evt) => {
-    const coordsPinMarker = evt.target.getLatLng();
-
-    return offerAddress.value = `${coordsPinMarker.lat.toFixed(5)}, ${coordsPinMarker.lng.toFixed(5)}`;
-
-  });
-
-  mainPinMarker.addTo(map);
+  renderSimilarCards(similarCards);
 
 };
 
-const createSimilarPins = (map) => {
-  const similarCards = createSimilarOffers(MAX_OFFERS_QUANTITY);
-
-  const createCustomPopup = (point) => {
-    const similarListElement = document.querySelector('#card').content.querySelector('.popup');
-    const popupElement = similarListElement.cloneNode(true);
-
-    const outputCard = getSimilarOfferElement(point);
-    popupElement.innerHTML='';
-
-    popupElement.append(outputCard);
-
-    return popupElement;
-  };
-
-  similarCards.forEach((point) => {
-    const {locationCoords: {lat, lng}} = point;
-
-    const similarPinIcon = L.icon({
-      iconUrl: '../img/pin.svg',
-      iconSize: [52, 52],
-      iconAnchor: [26, 52],
-    });
-    const marker = L.marker({
-      lat,
-      lng,
-    },
-    {
-      icon: similarPinIcon,
-    });
-
-    marker
-      .addTo(map)
-      .bindPopup(createCustomPopup(point));
-  });
+const resetMap = () => {
+  setMapView (initCoordinates.lat, initCoordinates.lng, initCoordinates.zoom);
+  setMainPinMarkerCoordinates(initCoordinates.lat, initCoordinates.lng);
+  map.closePopup();
 };
 
-const resetMap = (map, mainPinMarker) => {
-  mainPinMarker.setLatLng({
-    lat: 35.6895,
-    lng: 139.692,
-  });
-
-  map.setView({
-    lat: 35.6895,
-    lng: 139.692,
-  }, 16);
-};
-
-export {initMap, createSimilarPins, resetMap};
+export {initMap, resetMap, renderSimilarCards};
